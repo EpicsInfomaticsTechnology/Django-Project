4c45558eb93f314047cb5ee88d5bdd7c34151e3e
Aadith <aadithp210@gmail.com>
2022-08-20 10:20:45 +0530

    fix

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+with open("requirements.txt") as f:
+	install_requires = f.read().strip().split("\n")
+
+# get version from __version__ variable in djangoproject/__init__.py
+from djangoproject import __version__ as version
+
+setup(
+	name="djangoproject",
+	version=version,
+	description="ERPNext Laboratory Utility is an Frappe App for facilitating Laboratory",
+	author="epic",
+	author_email="aadithp210@gmail.com",
+	packages=find_packages(),
+	zip_safe=False,
+	include_package_data=True,
+	install_requires=install_requires
+)
